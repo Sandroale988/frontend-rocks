62e01c02d34bb6ac3c26e36bf43ee82279590b5f
Sandroale988 <alessandromarazzi1@gmail.com>
2025-03-06 11:04:00 +0000

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import viteLogo from "/vite.svg";
 
 export const App = () => {
   const [count, setCount] = useState(0);
+  const [title, setTitle] = useState("dipre commando");
 
   return (
     <div className="h-dvh flex flex-col items-center justify-center">
       <div className="bg-white p-8 rounded-md shadow-lg">
-        <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
-          Frontend Rocks
+        <h1 className="text-center font-bold text-3xl text-black-400 mb-4 cursor-pointer">
+          {title}
         </h1>
 
         <div className="flex justify-center space-x-4 mb-2">
@@ -21,21 +22,26 @@ export const App = () => {
           </a>
         </div>
 
-        <h2 className="text-center font-bold text-xl mb-6">Vite + React</h2>
+        <h2 className="text-center font-bold text-xl mb-6 cursor-pointer">dai uomooooo</h2>
 
         <div className="flex flex-col items-center space-y-4">
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
+            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-green-600 transition-colors "
             onClick={() => setCount((count) => count + 1)}
           >
             Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
           </button>
-          <p className="text-center">
+          <button
+          className="bg-red-500 text-black px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-yellow-600 transition-colors"
+          onClick={() => setTitle("dai uomo") }
+          >
+          dai uomo premimi
+        </button>
+          <p className="text-center cursor-pointer">
             Modifica <code>src/App.tsx</code> e salva per testare l'hot reload
           </p>
         </div>
-
-        <p className="text-center text-sm text-blue-400">Clicca sui loghi per saperne di più</p>
+        <p className="text-center text-sm text-blue-400 cursor-pointer">Clicca sui loghi per saperne di più</p>
       </div>
     </div>
   );
